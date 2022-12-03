7b11bf8ce9d626e33cab98d8f0a282f5b1c8bcc2
salgue441 <carlossalguero441@gmail.com>
2022-12-03 11:49:20 -0600

    Finished rptree logic

--- rptree/rptree.py
+++ rptree/rptree.py
@@ -10,10 +10,10 @@ SPACE_PREFIX = "   "
 
 
 class DirectoryTree:
-    def __init__(self, root_dir):
+    def __init__(self, root_dir) -> None:
         self._generator = _TreeGenerator(root_dir)
 
-    def generate(self):
+    def generate(self) -> str:
         tree = self._generator.build_tree()
 
         for entry in tree:
@@ -21,7 +21,7 @@ class DirectoryTree:
 
 
 class _TreeGenerator:
-    def __init__(self, root_dir):
+    def __init__(self, root_dir) -> None:
         """ Initialize the tree generator. 
             :param root_dir: The root directory of the tree.
             :type root_dir: str
@@ -31,7 +31,7 @@ class _TreeGenerator:
         self._root_dir = pathlib.Path(root_dir)
         self._tree = []
 
-    def build_tree(self):
+    def build_tree(self) -> list:
         """ Build the tree.
             :param None
             :return: The tree. """
@@ -41,7 +41,7 @@ class _TreeGenerator:
 
         return self._tree
 
-    def _tree_head(self):
+    def _tree_head(self) -> None:
         """ Build the tree head.
             :param None
             :return None
@@ -49,3 +49,69 @@ class _TreeGenerator:
 
         self._tree.append(f"{self._root_dir}{os.sep}")
         self._tree.append(PIPE)
+
+    def _tree_body(self, directory, prefix="") -> None:
+        """ Build the tree body.
+            :param directory: The directory to build the tree body for.
+            :type directory: str
+            :param prefix: The prefix to use for the tree body.
+            :type prefix: str
+            :return None
+        """
+
+        entries = directory.iterdir()
+        entries = sorted(entries, key=lambda entry: entry.is_file())
+        entries_count = len(entries)
+
+        for index, entry in enumerate(entries):
+            connector = ELBOW if index == entries_count - 1 else TEE
+
+            if entry.is_dir():
+                self.add_directory(
+                    entry, index, entries_count, prefix, connector
+                )
+
+            else:
+                self._add_file(entry, prefix, connector)
+
+    def add_directory(
+            self, directory, index, entries_count, prefix, connector
+    ) -> None:
+        """ Add a directory to the tree.
+            :param directory: The directory to add.
+            :type directory: str
+            :param index: The index of the directory.
+            :type index: int
+            :param entries_count: The number of entries in the directory.
+            :type entries_count: int
+            :param prefix: The prefix to use for the directory.
+            :type prefix: str
+            :param connector: The connector to use for the directory.
+            :type connector: str
+            :return None
+        """
+
+        self._tree.append(f"{prefix}{connector} {directory.name}{os.sep}")
+
+        if index != entries_count - 1:
+            prefix += PIPE_PREFIX
+
+        else:
+            prefix += SPACE_PREFIX
+
+        self._tree_body(directory=directory, prefix=prefix)
+
+        self._tree.append(prefix.rstrip())
+
+    def _add_file(self, file, prefix, connector) -> None:
+        """ Add a file to the tree.
+            :param file: The file to add.
+            :type file: str
+            :param prefix: The prefix to use for the file.
+            :type prefix: str
+            :param connector: The connector to use for the file.
+            :type connector: str
+            :return None
+        """
+
+        self._tree.append(f"{prefix}{connector} {file.name}")
